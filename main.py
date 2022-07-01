40b8ed69f2d68c9245fbac0a8d988d9bc049d84c
P0ulp1 <71771197+P0ulp1@users.noreply.github.com>
2022-07-01 19:08:52 +0200

    Create main.py
    Added the only file of the project.

---
+++ main.py
@@ -0,0 +1,6 @@
+import time
+print("Hello ! This is BandNameGenerator v1.0"), time.sleep(1)
+print("Launching the Software"), time.sleep(1), print("10%"), time.sleep(1), print("29%"), time.sleep(1), print("54%"), time.sleep(1), print("72%"), time.sleep(1), print("100%"), time.sleep(1)
+city = input("What is the city where you grew up to ?\n")
+pet = input("What is the name of your pet ?\n")
+print("The recommanded band name is : " + city + " " + pet)
